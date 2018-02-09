72b5243074e9a73be51694adc35e42f18e2bb803
Ben Gamber <ben@stickfishstudio.com>
2018-02-09 09:20:00 -0500

    Added platform collision for player! Allows jumping off platforms as well.

--- pygaman.py
+++ pygaman.py
@@ -9,33 +9,45 @@ class Pygaman(pygame.sprite.Sprite):
         super(Pygaman, self).__init__()
         self.x = x
         self.y = y
-        self.moveSpeed = 0
-        self.vertSpeed = 0
+        self.move_speed = 0
+        self.vert_speed = 0
         self.run1 = pygame.image.load('pygaman1.png').convert_alpha()
         self.run2 = pygame.image.load('pygaman2.png').convert_alpha()
         self.jump = pygame.image.load('pygaman3.png').convert_alpha()
         self.run_anim = [self.run1, self.run2]
         self.direction = 'right'
-        self.rect = self.run1.get_rect()
-        self.jumpCount = 0
+        self.rect = pygame.Rect(self.x, self.y, 30, 40)
+        self.jump_count = 0
 
-    def update(self, window):
-        self.x += self.moveSpeed
-
+    def update(self, window, platforms):
+        self.rect = pygame.Rect(self.x, self.y, 30, 40)
         self.gravity(window)
-        self.y += self.vertSpeed
+
+        for platform in platforms:
+            if self.rect.colliderect(platform.rect):
+                if self.rect.bottom >= (platform.rect.top + self.vert_speed) and \
+                self.rect.right >= platform.rect.left and self.rect.left <= platform.rect.right and \
+                self.vert_speed >= 0:
+                    self.y = (platform.rect.top - self.rect.height) + 1
+                    self.vert_speed = 0
+                    self.jump_count = 0
+        
+        self.x += self.move_speed
+        self.rect.x += self.move_speed
+        self.y += self.vert_speed
+        self.rect.y += self.vert_speed
 
     def render(self, frames=20, counter=0):
         frame = counter % frames
         sprite_index = frame / (frames / 2)
-        if self.vertSpeed != 0:
+        if self.vert_speed != 0:
             if self.direction == 'right':
                 return self.jump
             elif self.direction == 'left':
                 return pygame.transform.flip(self.jump, True, False)
-        elif self.moveSpeed > 0:
+        elif self.move_speed > 0:
             return self.run_anim[sprite_index]
-        elif self.moveSpeed < 0:
+        elif self.move_speed < 0:
             return pygame.transform.flip(self.run_anim[sprite_index], True, False)
         else:
             if self.direction == 'right':
@@ -44,11 +56,11 @@ class Pygaman(pygame.sprite.Sprite):
                 return pygame.transform.flip(self.run_anim[0], True, False)
 
     def gravity(self, window):
-        if (self.y + self.rect.height) >= (window.height - self.vertSpeed) and self.vertSpeed > 0:
-            self.vertSpeed = 0
-            self.jumpCount = 0
+        if (self.y + self.rect.height) >= (window.height - self.vert_speed) and self.vert_speed > 0:
+            self.vert_speed = 0
+            self.jump_count = 0
         elif (self.y + self.rect.height) < window.height:
-            self.vertSpeed += 0.5
+            self.vert_speed += 0.5
 
 class Pellet(pygame.sprite.Sprite):
     def __init__(self, x, y, direction, speedmod=0):
@@ -86,6 +98,7 @@ class Platform(pygame.sprite.Sprite):
         self.y = y
         self. width = width
         self.height = height
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
     def getRect(self):
         return (self.x, self.y, self.width, self.height)
@@ -106,6 +119,7 @@ def main():
     player = Pygaman(40, 550)
 
     pellets = pygame.sprite.Group()
+    platforms = pygame.sprite.Group()
 
     stage0 = [
         Platform(400, 600, 50, 10)
@@ -130,27 +144,32 @@ def main():
     counter = 0
     playing = True
     while playing:
+        if counter == 0:
+            for platform in platforms:
+                platform.kill()
+            for platform in stages[current_stage]:
+                platforms.add(platform)
         counter += 1
         for event in pygame.event.get():
             pressed = pygame.key.get_pressed()
             if pressed[pygame.K_RIGHT]:
                 player.direction = 'right'
-                player.moveSpeed = 3
+                player.move_speed = 3
             elif pressed[pygame.K_LEFT]:
                 player.direction = 'left'
-                player.moveSpeed = -3
+                player.move_speed = -3
             else:
-                player.moveSpeed = 0
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_UP and player.jumpCount < 2:
-                player.vertSpeed = -7
-                player.jumpCount += 1
+                player.move_speed = 0
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_UP and player.jump_count < 2:
+                player.vert_speed = -7
+                player.jump_count += 1
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                pellets.add(Pellet(player.x, player.y + 10, player.direction, speedmod=player.moveSpeed))
+                pellets.add(Pellet(player.x, player.y + 10, player.direction, speedmod=player.move_speed))
             if event.type == pygame.QUIT:
                 playing = False
 
         window.screen.fill(bg)
-        player.update(window)
+        player.update(window, platforms)
         window.screen.blit(player.render(counter=counter), (player.x, player.y))
         for pellet in pellets:
             pellet.update(window)
